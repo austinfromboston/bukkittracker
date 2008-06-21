6bcf333a6a6dfba240506e2504e56c18c93a9088
Austin Putman <austin@radicaldesigns.org>
2008-06-20 17:56:47 -0700

    created initial database migration

---
+++ db/migrate/20080621004653_initial_database.rb
@@ -0,0 +1,24 @@
+class InitialDatabase < ActiveRecord::Migration
+  def self.up
+    create_table "batches" do |t|
+      t.string    "filename"
+      t.datetime  "closed_at"
+      t.timestamps
+    end
+
+    create_table "bandwidth_usages" do |t|
+      t.integer "batch_id"
+      t.string  "source", "action", "units"
+      t.integer "amount"
+      t.datetime "completed_at"
+    end
+    add_index "bandwith_usages", ["batch_id"], :name => "index_bandwidth_usage_batch_id"
+  end
+
+  def self.down
+    remove_index "bandwith_usages", :name => "index_bandwidth_usage_batch_id"
+    drop_table "bandwidth_usages"
+
+    drop_table "batches"
+  end
+end
